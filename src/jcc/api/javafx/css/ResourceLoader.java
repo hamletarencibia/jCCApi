1a4840ec7fd032c038fd3ca67304ad46d59da032
hamletarencibia <hamletarencibia@gmail.com>
2021-08-21 14:17:36 -0700

    Constants are URL objects now

--- src/jcc/api/javafx/css/ResourceLoader.java
+++ src/jcc/api/javafx/css/ResourceLoader.java
@@ -4,8 +4,8 @@ import java.io.InputStream;
 import java.net.URL;
 
 public class ResourceLoader {
-	public static String BOOTSTRAP_4 = "bootstrap-4.css";
-	public static String BOOTSTRAP_5 = "bootstrap-5.css";
+	public static URL BOOTSTRAP_4 = get("bootstrap-4.css");
+	public static URL BOOTSTRAP_5 = get("bootstrap-5.css");
 	
 	public static URL get(String resource) {
 		return ResourceLoader.class.getResource(resource);
